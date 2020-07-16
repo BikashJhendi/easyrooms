4cacadb7abc77d7206478f69346e975cbb4f71e4
BikashJhendi <bikashjhendi11@gmail.com>
2020-07-16 19:01:14 +0545

    html update

--- roomsapps/views.py
+++ roomsapps/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+# index.html page
+def index(request):
+    return render(request, 'index.html')
+
+
+# aboutUs.html page
+def about_us(request):
+    return render(request, 'aboutUs.html')
+
+
