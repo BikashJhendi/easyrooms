bd548944b5482701306c56e7346669db3f8dbbf4
BikashJhendi <bikashjhendi11@gmail.com>
2020-08-29 19:52:09 +0545

    login updated

--- account/admin.py
+++ account/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from account.models import UsersAccount
-from django.contrib.auth.models import Group
 
 
 # Register your models here.
