f7e965da059acc0181e2c17d3f8a56f0dfe69a37
RajeshKasula <57468994+RajeshKasula@users.noreply.github.com>
2020-09-05 21:55:49 +0545

    search

--- roomsapps/static/js/search.js
+++ roomsapps/static/js/search.js
@@ -2,7 +2,7 @@ function search() {
  /* document.getElementById("search-room").classList.toggle("show");*/
     var x;
         x = document.getElementById("searchRoom").value;
-        if (x === "signup"||"Signup") {
-            window.open("signup");
+        if (x === "kathmandu"||"bhaktapur"||"pokhara"||"butwal") {
+            window.open("rooms");
         }
     }
